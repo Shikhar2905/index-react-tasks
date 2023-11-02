8968ac4932a447d208ec0339e220219ca77b4aae
Shikhar2905 <96392805+Shikhar2905@users.noreply.github.com>
2023-11-03 02:22:53 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -17,55 +17,55 @@ function App() {
           <tr>
             <td>my-react-app</td>
             <td>
-              <a href="https://github.com/Varun968/my-react-app">Link 1</a>
+              <a href="https://github.com/Shikhar2905/my-react-app">Link 1</a>
             </td>
             <td>
-              <a href="https://my-react-app-xi-one.vercel.app/">Link 1</a>
+              <a href="https://my-react-app-main.vercel.app/">Link 1</a>
             </td>
           </tr>
           <tr>
             <td>react-calculator-app</td>
             <td>
-              <a href="https://github.com/Varun968/react-calculator-app">Link 2</a>
+              <a href="https://github.com/Shikhar2905/react-calculator-app">Link 2</a>
             </td>
             <td>
-              <a href="https://react-calculator-app-mg3s.vercel.app/">Link 2</a>
+              <a href="https://react-calculator-app-opal.vercel.app/">Link 2</a>
             </td>
           </tr>
           <tr>
             <td>color-picker-react-app</td>
             <td>
-              <a href="https://github.com/Varun968/color-picker-react-app">Link 3</a>
+              <a href="https://github.com/Shikhar2905/color-picker-react-app">Link 3</a>
             </td>
             <td>
-              <a href="https://react-calculator-app-ashy.vercel.app/">Link 3</a>
+              <a href="https://color-picker-react-app.vercel.app/">Link 3</a>
             </td>
           </tr>
           <tr>
             <td>usertable-react-app</td>
             <td>
-              <a href="https://github.com/Varun968/usertable-react-app">Link 4</a>
+              <a href="https://github.com/Shikhar2905/usertable-react-app">Link 4</a>
             </td>
             <td>
-              <a href="https://usertable-react-app.vercel.app/">Link 4</a>
+              <a href="https://usertable-react-app-puce.vercel.app/">Link 4</a>
             </td>
           </tr>
           <tr>
             <td>age-calculator-react-app</td>
             <td>
-              <a href="https://github.com/Varun968/age-calculator-react-app">Link 5</a>
+              <a href="https://github.com/Shikhar2905/age-calculator-react-app">Link 5</a>
             </td>
             <td>
-              <a href="https://age-calculator-react-app-six.vercel.app/">Link 5</a>
+              <a href="https://age-calculator-react-app-tau.vercel.app/">Link 5</a>
             </td>
           </tr>
           <tr>
             <td>portfolio</td>
             <td>
-              <a href="https://github.com/Varun968/portfolio-react-app">Link 6</a>
+              <a href="https://github.com/Shikhar2905/portfolio-react-app">Link 6</a>
             </td>
             <td>
-              <a href="https://portfolio-react-app-rose.vercel.app/">Link 6</a>
+              <a href="https://portfolio-react-app-umber.vercel.app/">Link 6</a>
             </td>
           </tr>
         </tbody>
